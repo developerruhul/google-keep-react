ded2b84997fa3ecd019234bc54dd6bb4e08cd429
ruhulamin3482 <ruhulamin3482@gmail.com>
2017-11-30 10:37:04 +0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+module.hot.accept('./App.jsx', () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+})
+
+registerServiceWorker();
