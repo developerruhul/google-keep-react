f03dd49fa3b073fb6f1e9b4b78535a1d8241f2d6
ruhulamin3482 <ruhulamin3482@gmail.com>
2017-12-01 14:47:33 +0600

    everythings setUp at this point but not interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { HomePage } from "./scenes/index";
+import { SignScene } from "./scenes/index";
+
+
 
 export default class App extends React.Component {
   render() {
