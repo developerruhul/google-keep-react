f46d8d4ca5a0e4e81a965ede30c6816c40ceca01
iamruhul <ruhulamin3482@gmail.com>
2018-03-04 07:07:55 +0600

    app(route): HashRouter => BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,34 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { Reducers } from './store';
+import { Reducers } from "./store";
 import HomePage from "./scenes/home/App";
-import registerServiceWorker from './registerServiceWorker';
-
-
-
-
-
+import registerServiceWorker from "./registerServiceWorker";
 
 const store = createStore(
-    Reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  Reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-
-
-let render = () => ReactDOM.render(
+let render = () =>
+  ReactDOM.render(
     <Provider store={store}>
-        <HashRouter>
-            <HomePage />
-        </HashRouter>
+      <BrowserRouter>
+        <HomePage />
+      </BrowserRouter>
     </Provider>,
-    document.getElementById('root')
-);
-
-
+    document.getElementById("root")
+  );
 
 render();
 
