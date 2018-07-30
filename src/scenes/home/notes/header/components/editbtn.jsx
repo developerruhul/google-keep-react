40f01b4683e631b772aa32706f3ee7d6d35038ef
iamruhul <ruhulamin3482@gmail.com>
2018-07-30 18:48:28 +0600

    delete scene folder

--- src/scenes/home/notes/header/components/editbtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function EditButton({ toggle }) {
-    return (
-        <div onMouseDown={toggle} role="button" className="o-notes-header-btn js-note-edit">
-            EDIT
-        </div>
-    )
-}
