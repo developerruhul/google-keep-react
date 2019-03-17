f7839dce9c89bc5083d65ab695359582ba084fd2
Ruhul Amin <ruhulamin.webdev@gmail.com>
2019-03-17 20:53:22 +0600

    Updates

---
+++ precache-manifest.debc63fedbb768ba46057f3d6b3b70f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca8b21afd9e8c151136d",
+    "url": "/samsung-notes-web/static/css/main.c8ff67dc.chunk.css"
+  },
+  {
+    "revision": "ca8b21afd9e8c151136d",
+    "url": "/samsung-notes-web/static/js/main.337a3e53.chunk.js"
+  },
+  {
+    "revision": "b1b6c1e7813d853ab082",
+    "url": "/samsung-notes-web/static/js/runtime~main.22a1db55.js"
+  },
+  {
+    "revision": "ebbe9f9e850e4f1a4941",
+    "url": "/samsung-notes-web/static/js/2.5ff0a02e.chunk.js"
+  },
+  {
+    "revision": "d2b3ad4d808b83bd9608",
+    "url": "/samsung-notes-web/static/css/3.14669ab7.chunk.css"
+  },
+  {
+    "revision": "d2b3ad4d808b83bd9608",
+    "url": "/samsung-notes-web/static/js/3.84ce1989.chunk.js"
+  },
+  {
+    "revision": "6311981c58fe18ab6668",
+    "url": "/samsung-notes-web/static/css/4.7edea9fb.chunk.css"
+  },
+  {
+    "revision": "6311981c58fe18ab6668",
+    "url": "/samsung-notes-web/static/js/4.be4f392a.chunk.js"
+  },
+  {
+    "revision": "6aa4dcb71fb28378ccce2135f660dcf5",
+    "url": "/samsung-notes-web/index.html"
+  }
+];
