f648d1bab93cc67de42353c1572b2bb26657511c
ruhulamindev <ruhulamin.webdev@gmail.com>
2020-02-05 11:50:31 +0600

    upgrade packages and add basename in router

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-import Router from "./router";
-import "./index.css";
+import Router from './router';
+import './index.css';
+
+const baseName =
+  process.env.NODE_ENV === 'production' ? 'samsung-notes-web' : '';
 
 let render = () =>
   ReactDOM.render(
     <Provider store={store.store}>
-      <BrowserRouter>
+      <BrowserRouter basename={baseName}>
         <PersistGate loading={null} persistor={store.persistor}>
           <Router />
         </PersistGate>
       </BrowserRouter>
     </Provider>,
-    document.getElementById("root")
+    document.getElementById('root')
   );
 
 render();
 
 if (module.hot) {
-  module.hot.accept("./router", () => {
+  module.hot.accept('./router', () => {
     render();
   });
 }
--- src/router.js
+++ src/router.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import asyncComponent from "./util/AsyncFunc";
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import asyncComponent from './util/AsyncFunc';
 
 class Router extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route
-            path="/dashboard"
-            component={asyncComponent(() => import("./containers"))}
-          />
-          <Route render={() => <Redirect to="/dashboard" />} />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route
+          path='/dashboard'
+          component={asyncComponent(() => import('./containers'))}
+        />
+        <Route render={() => <Redirect to='/dashboard' />} />
+      </Switch>
     );
   }
 }
