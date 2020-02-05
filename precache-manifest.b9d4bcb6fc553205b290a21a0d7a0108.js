798c3596832b023d49aae5aa83f65c4202da818e
ruhulamindev <ruhulamin.webdev@gmail.com>
2020-02-05 11:50:28 +0600

    Updates

---
+++ precache-manifest.b9d4bcb6fc553205b290a21a0d7a0108.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b36ac25ff6da54a71aea8e374b1b4e",
+    "url": "/samsung-notes-web/index.html"
+  },
+  {
+    "revision": "86cc8f753e15a253bd88",
+    "url": "/samsung-notes-web/static/css/2.0d10559b.chunk.css"
+  },
+  {
+    "revision": "c00ec8706bd94d0155ef",
+    "url": "/samsung-notes-web/static/css/4.e201c97a.chunk.css"
+  },
+  {
+    "revision": "ad52ddbfcb36490c4a97",
+    "url": "/samsung-notes-web/static/css/main.830e21e6.chunk.css"
+  },
+  {
+    "revision": "86cc8f753e15a253bd88",
+    "url": "/samsung-notes-web/static/js/2.106acc34.chunk.js"
+  },
+  {
+    "revision": "472da389bdfd95fc7186800476f71ef6",
+    "url": "/samsung-notes-web/static/js/2.106acc34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "705bffe5b3d52239f58d",
+    "url": "/samsung-notes-web/static/js/3.d845d01a.chunk.js"
+  },
+  {
+    "revision": "f42ae6e4b4a90a4b7e3fd85ee7befc56",
+    "url": "/samsung-notes-web/static/js/3.d845d01a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c00ec8706bd94d0155ef",
+    "url": "/samsung-notes-web/static/js/4.2e0545e9.chunk.js"
+  },
+  {
+    "revision": "ad52ddbfcb36490c4a97",
+    "url": "/samsung-notes-web/static/js/main.6cd8140b.chunk.js"
+  },
+  {
+    "revision": "ef9ccfe202c810670da5",
+    "url": "/samsung-notes-web/static/js/runtime-main.0b58b259.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/samsung-notes-web/precache-manifest.debc63fedbb768ba46057f3d6b3b70f4.js"
+  "/samsung-notes-web/precache-manifest.b9d4bcb6fc553205b290a21a0d7a0108.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/samsung-notes-web/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/samsung-notes-web/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
